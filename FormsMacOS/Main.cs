7b477d933f0028e7e30b428b21bf16f9a91587e5
ledangvu <ledangvu@gmail.com>
2021-07-17 23:07:30 +0700

    update for mac, from now on we can run app on MAC

--- FormsMacOS/Main.cs
+++ FormsMacOS/Main.cs
@@ -7,6 +7,7 @@ namespace FormsMacOS
 		static void Main(string[] args)
 		{
 			NSApplication.Init();
+			NSApplication.SharedApplication.Delegate = new AppDelegate();
 			NSApplication.Main(args);
 		}
 	}
