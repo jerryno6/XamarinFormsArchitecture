a1ae460555c3789f07144ab37385347bca6cddc4
ledangvu <ledangvu@gmail.com>
2021-07-18 13:10:20 +0700

    add backend grpc service

---
+++ FormsArchitecture.Service/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.Hosting;
+
+namespace FormsArchitecture.Service
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			CreateHostBuilder(args).Build().Run();
+		}
+
+		// Additional configuration is required to successfully run gRPC on macOS.
+		// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+		public static IHostBuilder CreateHostBuilder(string[] args) =>
+			Host.CreateDefaultBuilder(args)
+				.ConfigureWebHostDefaults(webBuilder =>
+				{
+					webBuilder.UseStartup<Startup>();
+
+
+//#if DEBUG
+//					//todo: only use this to run in MACOS as develop environment
+//					webBuilder.ConfigureKestrel(options =>
+//					{
+//						options.Limits.MinRequestBodyDataRate = null;
+
+//						options.ListenAnyIP(35960,
+//							listenOptions => { listenOptions.Protocols = HttpProtocols.Http1; });
+
+//						options.ListenAnyIP(35961,
+//							listenOptions => { listenOptions.Protocols = HttpProtocols.Http2; });
+//					});
+//#endif
+				});
+	}
+}
